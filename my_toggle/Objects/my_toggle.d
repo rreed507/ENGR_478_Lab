c7a97ffd6717a48a30226d49213eb5418b3742be
rreed507 <61760407+rreed507@users.noreply.github.com>
2020-03-05 20:41:42 -0800

    1

--- my_toggle/Objects/my_toggle.d
+++ my_toggle/Objects/my_toggle.d
@@ -1,5 +1,3 @@
 .\objects\my_toggle.o: my_toggle.c
 .\objects\my_toggle.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 .\objects\my_toggle.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
-.\objects\my_toggle.o: ..\..\..\..\inc/tm4c123gh6pm.h
-.\objects\my_toggle.o: ..\..\..\..\driverlib/sysctl.h
